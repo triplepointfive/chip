266b111953c00b77b67cf3706805ff4d68d613a4
Ilya Smelkov <ismelkov@restream.rt.ru>
2019-05-14 13:41:23 +0300

    Add spec 😔

--- test/Main.purs
+++ test/Main.purs
@@ -2,9 +2,27 @@ module Test.Main where
 
 import Prelude
 
-import Effect
-import Test.Assert (assert)
+import Data.Time.Duration (Milliseconds(..))
+import Effect (Effect)
+import Effect.Aff (launchAff_, delay)
+import Test.Spec (pending, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (run)
 
 main :: Effect Unit
-main = do
-  assert (123 == 233167)
+main = run [consoleReporter] do
+  describe "purescript-spec" do
+    describe "Attributes" do
+      it "awesome" do
+        let isAwesome = true
+        isAwesome `shouldEqual` true
+      pending "feature complete"
+    describe "Features" do
+      it "runs in NodeJS" $ pure unit
+      it "runs in the browser" $ pure unit
+      it "supports streaming reporters" $ pure unit
+      it "supports async specs" do
+        res <- delay (Milliseconds 100.0) $> "Alligator"
+        res `shouldEqual` "Alligator"
+      it "is PureScript 0.12.x compatible" $ pure unit
